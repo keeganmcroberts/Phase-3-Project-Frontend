1e5368075d863ad24ad240a814e3924152fdd402
Caroline <ckmcdon@gmail.com>
2022-08-03 13:00:08 -0400

    updates to EditExpense

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,23 +1,73 @@
 import {useState} from "react";
 
-function EditExpense () {
+function EditExpense ({receiveNewExpense}) {
+const [priceInput, setPriceInput] = useState("")
+const [categoryInput, setCategoryInput] = useState("")
+const [nameInput, setNameInput] = useState("")
+const [dateInput, setDateInput] = useState("")
+
 
     return (
         <div className="editExpenseClass">
              <h2>New Expense</h2> 
-                    <form>
+                    <form
+                        onSubmit={(e)=>{
+                            e.preventDefault()
+                        let newExpense = {
+                            amount: priceInput,
+                            category_id: categoryInput,
+                            date: dateInput,
+                        }
+                        receiveNewExpense(newExpense)
+                        }}>
                         <input
                         type="text"
-                        name="insertPrice"
-                        placeholder="Insert Price"
+                        name="date"
+                        placeholder="Date"
+                        value={dateInput}
+                        onChange={(e)=>{setDateInput(e.target.value)}}
                         />
                         <input
+                        type="text"
+                        name="expense"
+                        placeholder="Price"
+                        value={priceInput}
+                        onChange={(e)=>{setPriceInput(e.target.value)}}
+                        />
+                        <input
+                        type="text"
+                        name="nameOfExpense"
+                        placeholder="Name"
+                        value={nameInput}
+                        onChange={(e)=>{setNameInput(e.target.value)}}
+                        />
+                        <select 
                         type="text"
                         name="category"
                         placeholder="Category"
+                        value={categoryInput}
+                        onChange={(e)=>{setCategoryInput(e.target.value)}}
+                        >
+                            <option value="category">Category</option>
+                            <option value="rent">Rent</option>
+                            <option value="food">Food</option>
+                            <option value="clothes">Clothes</option>
+                            <option value="vacation">Vacation</option>
+                            <option value="gas">Gas</option>
+                            <option value="doctorBill">Doctor Bill</option>
+                            <option value="waterBill">Water Bill</option>
+                            <option value="powerBill">Power Bill</option>
+                            <option value="internetBill">Internet Bill</option>
+                            <option value="phoneBill">Phone Bill</option>
+                            
+                        </select> 
+                        <input
+                        type="submit"
+                        name="submit"
+                        value="Save"
+                        id="saveButton"
                         />
                     </form>
-            <button id="saveButton">Save</button>
 
             <h2>Edit Expense</h2>
                 <div className="expenseSearchBar">
@@ -25,7 +75,7 @@ function EditExpense () {
                     className="searchBar"
                     name="searchBar"
                     placeholder="Search"
-                    // onChange={(e)=>{getSearchValues(e.target.value)}}
+                    // onChange={(e)=>{receiveExpenseToRemove(e.target.value)}}
                     />
                 </div>
                 <button id="removeButton">Remove</button>
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,36 @@
 import EditExpense from "./EditExpense";
 import MonthlyExpense from "./MonthlyExpense";
+import {useState, useEffect} from "react";
 
 
 function MainContainer(){
+const [expenses, setNewExpenses] = useState([])
+
+
+useEffect(() => {
+    fetch('http://localhost:9292/expenses')
+    .then(res=>res.json())
+    .then(data=>{setNewExpenses(data)})
+}, [])
+
+    function receiveNewExpense(newExpense) {
+        fetch("http://localhost:9292/expenses", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newExpense)
+        })
+        setNewExpenses([newExpense, ...expenses])
+    }
+
+    // function receiveExpenseToRemove(removeExpense)
+    //     fetch("http://localhost:9292/expenses", {
+    //         method: 'DELETE'
+    //     })
+
+
     return (
         <div className="mainContainerClass">
-            <EditExpense/>
+            <EditExpense receiveNewExpense={receiveNewExpense} />
             <MonthlyExpense/>
         </div>
     )
