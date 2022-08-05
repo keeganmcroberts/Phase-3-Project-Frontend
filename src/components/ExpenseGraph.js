8eebea2383c162823c325658a793923f3a4a3b45
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-08-05 07:44:20 -0500

    idk

--- src/components/ExpenseGraph.js
+++ src/components/ExpenseGraph.js
@@ -2,10 +2,12 @@ import React from "react";
 import { VictoryPie } from "victory-pie";
 
 const myData = [
-  { x: "Bills", y: 900 },
+  { x: "Bills", y: 300 },
   { x: "Food", y: 400 },
   { x: "Gas", y: 300 },
-  { x: "Clothes", y: 300}
+  { x: "Clothes", y: 300},
+  { x: "Rent", y: 900},
+  { x: "Vacation", y: 300},
 ];
 
 const ExpenseGraph = () => {
@@ -13,7 +15,7 @@ const ExpenseGraph = () => {
     <div className="pieChart">
       <VictoryPie
         data={myData}
-        colorScale={["blue", "yellow", "red", "green"]}
+        colorScale={["blue", "yellow", "red", "green", "orange", "purple"]}
         radius={60}
       />
     </div>
